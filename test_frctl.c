317cd447a3aef2754e71edba164de74ee0da4875
zhy2on <dhwlgus5940@gmail.com>
2022-01-30 00:02:51 +0900

    test frctl

---
+++ test_frctl.c
@@ -0,0 +1,168 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_frctl.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 16:53:19 by jihoh             #+#    #+#             */
+/*   Updated: 2022/01/30 00:02:00 by jihoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_frctl.h"
+#include <stdio.h>
+
+int	mousewheel_hook(int button, int x, int y, t_frctl *frctl)
+{
+	double	z;
+	t_cmplx	before;
+	t_cmplx	after;
+	t_point	point;
+
+	z = 1.05f;
+	point.x = x;
+	point.y = y;
+	if (button == ON_MOUSEDOWN)
+	{
+		screen_to_world(&point, &before, frctl);
+		frctl->zoom *= z;
+		screen_to_world(&point, &after, frctl);
+		frctl->offx += before.r - after.r;
+		frctl->offy += before.i - after.i;
+		draw_fractol(frctl, &frctl->data);
+	}
+	else if (button == ON_MOUSEUP)
+	{
+		screen_to_world(&point, &before, frctl);
+		z = 1.0 / z;
+		frctl->zoom *= z;
+		screen_to_world(&point, &after, frctl);
+		printf("before after: %f %f %f %f\n", before.r, before.i, after.r, after.i);
+		frctl->offx += before.r - after.r;
+		frctl->offy += before.i - after.i;
+		draw_fractol(frctl, &frctl->data);
+	}
+	return (0);
+}
+
+void	put_color(t_data *data, t_point point, t_clr clr)
+{
+	int	pos;
+
+	if (point.x < 0 || point.y < 0 || point.x >= WIN_W || point.y >= WIN_H)
+		return ;
+	pos = point.x * data->bpp / 8 + point.y * data->bpl;
+	data->buff[pos] = clr.b;
+	data->buff[pos + 1] = clr.g;
+	data->buff[pos + 2] = clr.r;
+}
+
+int	key_press(int keycode, t_frctl *frctl)
+{
+	double	offset;
+
+	offset = 0.1f * frctl->zoom;
+	if (keycode == KEY_ESC)
+		exit(0);
+	else if (keycode == UP_ARROW)
+	{
+		frctl->offy -= offset;
+		draw_fractol(frctl, &frctl->data);
+	}
+	else if (keycode == DOWN_ARROW)
+	{
+		frctl->offy += offset;
+		draw_fractol(frctl, &frctl->data);
+	}
+	else if (keycode == LEFT_ARROW)
+	{
+		frctl->offx -= offset;
+		draw_fractol(frctl, &frctl->data);
+	}
+	else if (keycode == RIGHT_ARROW)
+	{
+		frctl->offx += offset;
+		draw_fractol(frctl, &frctl->data);
+	}
+	return (0);
+}
+
+void	screen_to_world(t_point *point, t_cmplx *cmplx, t_frctl *fr)
+{
+	double	xscale;
+	double	yscale;
+
+	xscale = (double)(fr->xmax - fr->xmin) / WIN_W;
+	yscale = (double)(fr->ymax - fr->ymin) / WIN_H;
+	cmplx->r = point->x * xscale * fr->zoom + fr->xmin + fr->offx;
+	cmplx->i = point->y * yscale * fr->zoom + fr->ymin + fr->offy;
+}
+
+void	mandelbrot(t_frctl *frctl, t_data *data, t_point point)
+{
+	t_clr	clr;
+	t_cmplx	c;
+	t_cmplx	z;
+	t_cmplx	tmp;
+	int		iter;
+
+	screen_to_world(&point, &c, frctl);
+	screen_to_world(&point, &z, frctl);
+	iter = -1;
+	while (++iter < frctl->itermax)
+	{
+		tmp = z;
+		z.r = tmp.r * tmp.r - tmp.i * tmp.i + c.r;
+		z.i = 2 * tmp.r * tmp.i + c.i;
+		if (z.r * z.r + z.i * z.i > 4.0)
+			break ;
+	}
+	clr.r = (frctl->itermax - iter) % 255;
+	clr.g = (frctl->itermax - iter) % 255 + 10;
+	clr.b = (frctl->itermax - iter) % 255 + 20;
+	put_color(data, point, clr);
+}
+
+void	draw_fractol(t_frctl *frctl, t_data *data)
+{
+	t_point	point;
+
+	point.y = -1;
+	while (++point.y < WIN_H)
+	{
+		point.x = -1;
+		while (++point.x < WIN_W)
+			mandelbrot(frctl, data, point);
+	}
+	mlx_put_image_to_window(frctl->mlx, frctl->win, data->img, 0, 0);
+}
+
+void	init_vars(t_frctl *frctl)
+{
+	frctl->mlx = mlx_init();
+	frctl->win = mlx_new_window(frctl->mlx, WIN_W, WIN_H, "mlx 42");
+	frctl->data.img = mlx_new_image(frctl->mlx, WIN_W, WIN_H);
+	frctl->data.buff = mlx_get_data_addr(frctl->data.img, &frctl->data.bpp,
+			&frctl->data.bpl, &frctl->data.endian);
+	frctl->xmin = -2.5;
+	frctl->xmax = 1;
+	frctl->ymin = -1;
+	frctl->ymax = 1;
+	frctl->offx = 0;
+	frctl->offy = 0;
+	frctl->zoom = 1;
+	frctl->itermax = 128;
+}
+
+int	main(void)
+{
+	t_frctl	frctl;
+
+	init_vars(&frctl);
+	mlx_hook(frctl.win, ON_KEYDOWN, 1L << 0, key_press, &frctl);
+	mlx_hook(frctl.win, ON_MOUSEDOWN, 1L << 2, mousewheel_hook, &frctl);
+	draw_fractol(&frctl, &frctl.data);
+	mlx_loop(frctl.mlx);
+	return (0);
+}
---
+++ test_frctl.h
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_frctl.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 15:56:36 by jihoh             #+#    #+#             */
+/*   Updated: 2022/01/29 14:55:34 by jihoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_FRCTL_H
+# define TEST_FRCTL_H
+
+# include <stdlib.h>
+# include <mlx.h>
+
+# define WIN_W	960
+# define WIN_H	540
+
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17,
+	KEY_ESC = 53,
+	LEFT_ARROW= 123,
+	RIGHT_ARROW = 124,
+	UP_ARROW = 126,
+	DOWN_ARROW = 125,
+	LEFT_CLICK = 1
+};
+
+typedef struct s_clr
+{
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+}				t_clr;
+
+typedef struct s_cmplx
+{
+	double	r;
+	double	i;
+}				t_cmplx;
+
+typedef struct s_point {
+	int	x;
+	int	y;
+}				t_point;
+
+typedef struct s_data {
+	void	*img;
+	char	*buff;
+	int		bpp;
+	int		bpl;
+	int		endian;
+}				t_data;
+
+typedef struct s_frctl {
+	void	*mlx;
+	void	*win;
+	double	xmin;
+	double	xmax;
+	double	ymin;
+	double	ymax;
+	double	offx;
+	double	offy;
+	double	zoom;
+	int		itermax;
+	t_data	data;
+}				t_frctl;
+
+void	screen_to_world(t_point *point, t_cmplx *cmplx, t_frctl *fr);
+void	init_vars(t_frctl *frctl);
+int		key_press(int keycode, t_frctl *vars);
+void	put_color(t_data *data, t_point point, t_clr clr);
+void	mandelbrot(t_frctl *frctl, t_data *data, t_point point);
+void	draw_fractol(t_frctl *frctl, t_data *data);
+void	zoom(t_frctl *frctl, t_data *data, int x, int y);
+int		mousewheel_hook(int button, int x, int y, t_frctl *frctl);
+
+#endif
